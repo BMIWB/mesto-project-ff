24d68c0b546d42b8b97bdb64df4df2879eda6388
Quino <michurinec007@gmail.com>
2025-01-19 18:54:38 +0300

    First try

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+// Исходный массив карточек
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+const cardContainer = document.querySelector('.places__list');
+
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+  
+  return cardElement;
+}
+
+function deleteCard(event) {
+  event.target.closest('.card').remove();
+}
+
+initialCards.forEach((dataCard) => {
+  const cardElement = createCard(dataCard, deleteCard);
+  cardContainer.append(cardElement);
+});
